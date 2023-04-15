4317a4cd78365340b059164a100e536b95de7da0
Hedi <hedi@sellami.dev>
2023-04-15 23:35:44 +0200

    Fix AnimationComponent

--- Board/Pawn/Component/LocomotionComponent.cs
+++ Board/Pawn/Component/LocomotionComponent.cs
@@ -16,42 +16,44 @@ public partial class LocomotionComponent : Component
         RunMovement(target - Pawn.Position);
     }
 
-    private bool isRunning;
-
     public void RunMovement(Vector2 movement)
     {
         if (isRunning)
             throw (new Exception("LocomotionComponent is already running"));
         isRunning = true;
-        PositionStart = Pawn.Position;
-        Movement = movement;
+        positionStart = Pawn.Position;
+        this.movement = movement;
         var animation = Pawn.Get<AnimationComponent>();
         animation.AnimationPlayer.Play("move");
-        if (Movement.X > 0)
+        if (this.movement.X > 0)
             animation.LookRight();
-        else if (Movement.X < 0)
+        else if (this.movement.X < 0)
             animation.LookLeft();
-        animation.AnimationPlayer.AnimationFinished +=
-            (_ =>
-                {
-                    isRunning = false;
-                    Pawn.Position = PositionStart + Movement;
-                    EndMovementEvent?.Invoke();
-                }
-            );
+
+        var onAnimationFinished = new AnimationPlayer.AnimationFinishedEventHandler(_ =>
+            {
+                isRunning = false;
+                Pawn.Position = positionStart + this.movement;
+                EndMovementEvent?.Invoke();
+            }
+        );
+        animation.AnimationPlayer.AnimationFinished += onAnimationFinished;
+        EndMovementEvent += () => animation.AnimationPlayer.AnimationFinished -= onAnimationFinished;
     }
 
     public event Action EndMovementEvent;
 
-    Vector2 PositionStart;
-    Vector2 Movement;
+    private bool isRunning;
+
+    Vector2 positionStart;
+    Vector2 movement;
 
     public override void _Process(double delta)
     {
         base._Process(delta);
         if (isRunning)
         {
-            Pawn.Position = PositionStart + Movement * MovementProgress;
+            Pawn.Position = positionStart + movement * MovementProgress;
         }
     }
     override protected IEnumerable<Type> GetDependencies()
